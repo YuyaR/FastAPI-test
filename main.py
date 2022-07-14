d927cb9dfccefa114d320fe28401fb30633cf16a
Yuya Ra <yuya.ra@limejump.com>
2022-07-14 18:06:51 +0100

    Fix: response body typing

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ class ItemWithoutTax(BaseModel):
 
 class ItemOut(BaseModel):
 	message: str
-	item_info: List[ItemWithoutTax]
+	item_info: Union[List[ItemWithoutTax], ItemWithoutTax]
 
+class ItemDel(BaseModel):
+	item_id: int
+	# message: str = None
 
 items_list = [
 	{"item_id": 1, "name": "Foo", "description": 'generic item', "price": 5, "tax": 1},
@@ -48,22 +51,23 @@ async def read_item(item_q):
 	return read_item_by_id(int(item_q))
 
 def read_item_by_id(item_id: int):
-	print("item_id", item_id)
 	if item_id not in [itm["item_id"] for itm in items_list]:
 		raise HTTPException(status_code=404, detail="Item not in the warehouse")
-	return list(filter(lambda item: item["item_id"] == item_id, items_list))
+	item_filter = {"item_id": item_id}
+	right_item = [dic for dic in items_list if all(dic[key] == val for key, val in item_filter.items())]
+	return right_item
 
 def read_item_by_name(name: str):
-	print("by name", name)
 	if name not in [itm["name"] for itm in items_list]:
 		raise HTTPException(status_code=404, detail="Item with this name not found")
-	return list(filter(lambda item: item["name"] == name, items_list))
+	item_filter = {"name": name}
+	right_item = [dic for dic in items_list if all(dic[key] == val for key, val in item_filter.items())]
+	return right_item
 
-@app.post("/items/create", response_model=ItemOut, status_code=201)
+@app.post("/items/", response_model=ItemOut, status_code=201)
 async def create_item(
 	item: ItemIn = Body(
 		example = {
-			"item_id": 88,
 			"name": "yuya",
 			"description": "a junior data analyst",
 			"price":10,
@@ -82,7 +86,7 @@ async def create_item(
 		"item_info": item_dict
 	}
 
-
-# @app.get("/files/{file_path:path}")
-# async def read_file(file_path: str):
-# 	return {"file_path": file_path}
+@app.delete("/items/", status_code=202)
+async def delete_item(item: ItemDel):
+	item_dict = dict(item)
+	[items_list.remove(dic) for dic in items_list if (dic['item_id'] == item_dict['item_id'])]
