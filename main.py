ad3cfcce67a1f9aa69f45c1569263dc6bb43a23a
Yuya Ra <yuya.ra@limejump.com>
2022-07-14 12:17:54 +0100

    Feat: add and read items by id and name

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from enum import Enum
-from typing import Union
+from typing import Union, List
 
 from fastapi import FastAPI, Path, Body, HTTPException
 from pydantic import BaseModel
 
-
-class ModelName(str, Enum):
-	alexnet = "alexnet"
-	resnet = "resnet"
-	lenet = "lenet"
 
 class ItemIn(BaseModel):
 	name: str
@@ -16,69 +11,78 @@ class ItemIn(BaseModel):
 	price: float
 	tax: float = None
 
-class ItemOut(BaseModel):
+class ItemWithoutTax(BaseModel):
+	item_id: int
 	name: str
 	description: Union[str, None] = None
 	price: float
 
-items_list = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+class ItemOut(BaseModel):
+	message: str
+	item_info: List[ItemWithoutTax]
+
+
+items_list = [
+	{"item_id": 1, "name": "Foo", "description": 'generic item', "price": 5, "tax": 1},
+	{"item_id": 2, "name": "Bar", "description": 'generic item', "price": 7},
+	{"item_id": 3, "name": "Baz", "description": 'generic item', "price": 3, "tax": 0.3},
+]
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-	return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-	return {"item_id:": item_id}
-
-@app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-	if model_name == ModelName.alexnet:
-		return{"model_name": model_name}
-	elif model_name.value == "lenet":
-		return{"model_name": model_name}
-	else:
-		return{"message": "no such model available"}
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-	return {"file_path": file_path}
+	return {"message": "Welcome to Yuya's warehouse"}
 
 @app.get("/items/")
-async def read_item_list(skip: int = 0, limit: int = 0):
+async def read_item_list(skip: int = 0, limit: int = 100):
 	return items_list[skip : skip + limit]
 
-@app.get("/items/name/")
-async def read_item_name(loc: int = 0, size: int = None):
-	if loc >= len(items_list):
-		raise HTTPException(status_code=404, detail="Item not found")
-	if size:
-		return {"item_name": items_list[loc]["item_name"], "size": size}
-	return {"item_name": items_list[loc]["item_name"], "size": "Free"}
+@app.get("/items/{item_q}")
+async def read_item(item_q):
+	try:
+		int(item_q)
+	except ValueError:
+		return read_item_by_name(item_q)
+	return read_item_by_id(int(item_q))
 
-@app.post("/users/{user_id}/items/", response_model=ItemOut, status_code=201)
+def read_item_by_id(item_id: int):
+	print("item_id", item_id)
+	if item_id not in [itm["item_id"] for itm in items_list]:
+		raise HTTPException(status_code=404, detail="Item not in the warehouse")
+	return list(filter(lambda item: item["item_id"] == item_id, items_list))
+
+def read_item_by_name(name: str):
+	print("by name", name)
+	if name not in [itm["name"] for itm in items_list]:
+		raise HTTPException(status_code=404, detail="Item with this name not found")
+	return list(filter(lambda item: item["name"] == name, items_list))
+
+@app.post("/items/create", response_model=ItemOut, status_code=201)
 async def create_item(
-	*,
-	user_id: int = Path(title="Student ID of User", default=1),
 	item: ItemIn = Body(
 		example = {
-				"name": "yuya",
-				"description": "a junior data analyst",
-				"price":11.2,
-				"tax":1.2,
+			"item_id": 88,
+			"name": "yuya",
+			"description": "a junior data analyst",
+			"price":10,
+			"tax":1.2,
 		}
 	)
 ):
 	if item.tax:
 		item.price += item.tax
-	return item
+	id_list = [itm["item_id"] for itm in items_list]
+	item_dict = item.dict()
+	item_dict.update(item_id = max(id_list)+1)
+	items_list.append(item_dict)
+	return {
+		"message": "new item added successfully",
+		"item_info": item_dict
+	}
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_owned_items(user_id: int, item_id: int, short: bool = True):
-	item = {"user_id": user_id, "item_id": item_id}
-	if short is False:
-		item.update({"description": "This is just extra words"})
-	return item
+
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+# 	return {"file_path": file_path}
